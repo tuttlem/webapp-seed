ae810833ff62cf1ae4031c19f45b42b3326c5b2e
Michael Tuttle <tuttlem@gmail.com>
2015-12-15 21:42:52 +1000

    Changed server over to be node

--- gulpfile.js
+++ gulpfile.js
@@ -9,56 +9,53 @@ var gulp = require('gulp'),
 /* javascripts need to be converted from 6 to 5 and minified */
 gulp.task('js', function () {
 
-    return gulp.src(['app/js/**/*.js'])
-        .pipe(replace('bower_components', 'lib'))
-        .pipe(gulp.dest('public/js'))
-        .pipe(babel())
-        .pipe(uglify());
+  return gulp.src(['app/js/**/*.js'])
+    .pipe(babel())
+    .pipe(uglify())
+    .pipe(replace('bower_components', 'lib'))
+    .pipe(gulp.dest('public/js'));
 
 });
 
 /* copy all static assets into the build */
 gulp.task('static', function () {
 
-    gulp.src('app/*.html')
-        .pipe(replace('bower_components', 'lib'))
-        .pipe(gulp.dest('public'));
+  gulp.src('app/*.html')
+    .pipe(replace('bower_components', 'lib'))
+    .pipe(gulp.dest('public'));
 
-    gulp.src('app/bower_components/**')
-        .pipe(gulp.dest('public/lib'))
+  gulp.src('app/bower_components/**')
+    .pipe(gulp.dest('public/lib'))
 
-    gulp.src('app/img/**')
-        .pipe(gulp.dest('public/img'));
+  gulp.src('app/img/**')
+    .pipe(gulp.dest('public/img'));
 
-    gulp.src('app/css/**')
-        .pipe(gulp.dest('public/css'));
+  gulp.src('app/css/**')
+    .pipe(gulp.dest('public/css'));
 
-    gulp.src('app/partials/**')
-        .pipe(gulp.dest('public/partials'));
+  gulp.src('app/partials/**')
+    .pipe(gulp.dest('public/partials'));
 
 });
 
 gulp.task('serve', function () {
 
-    gulp.src('public')
-        .pipe(webserver({
-            host: '0.0.0.0',
-            port: 3000,
-            fallback: 'index.html',
-            livereload: true,
-            open: true
-        }));
+  nodemon({
+    script: 'index.js',
+    ext: 'js css html',
+    ignore: [ 'public' ]
+  });
 
 });
 
 gulp.task('watch', function () {
-    gulp.watch('app/*.html', [ 'static' ]);
-    gulp.watch('app/bower_components/**', [ 'static' ]);
-    gulp.watch('app/img/**', [ 'static' ]);
-    gulp.watch('app/css/**', [ 'static' ]);
-    gulp.watch('app/partials/**', [ 'static' ]);
+  gulp.watch('app/*.html', [ 'static' ]);
+  gulp.watch('app/bower_components/**', [ 'static' ]);
+  gulp.watch('app/img/**', [ 'static' ]);
+  gulp.watch('app/css/**', [ 'static' ]);
+  gulp.watch('app/partials/**', [ 'static' ]);
 
-    gulp.watch('app/js/**/*.js', [ 'js' ]);
+  gulp.watch('app/js/**/*.js', [ 'js' ]);
 });
 
-gulp.task('default', [ 'js', 'static', 'watch', 'serve' ]); 
+gulp.task('default', [ 'js', 'static', 'serve', 'watch' ]); 
